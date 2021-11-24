cda1f65406a43afd0f15d43cc3c33c2c278f0cb7
Junha <prodopark15@gmail.com>
2021-11-24 22:49:51 +0900

    practice 11m4w

--- 21년 11월 4주차/백준_1003피보나치함수.py
+++ 21년 11월 4주차/백준_1003피보나치함수.py
@@ -14,3 +14,18 @@ def fibo(num):
 for i in range(n):
     num = int(input())
     fibo(num)
+
+# 내가 푼 것
+n = int(input())
+
+zero =[1, 0, 1]
+one = [0, 1, 1]
+
+for i in range(n):
+    num = int(input())
+    length = len(zero)
+    if length <= num:
+        for j in range(length, num+1):
+            zero.append(zero[j-1] + zero[j-2])
+            one.append(one[j-1] + one[j-2])
+    print(zero[num],one[num])
