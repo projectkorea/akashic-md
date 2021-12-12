a185c2efe12271a9ec7040dd3dbe3b1ec6b1856a
Junha <prodopark15@gmail.com>
2021-12-12 22:49:25 +0900

    update async

--- examples/3.promise.js
+++ examples/3.promise.js
@@ -1,44 +1,88 @@
-const getHen = () =>
-    new Promise((resolve, reject) => {
-        setTimeout(() => resolve('🐔'), 1000);
-    });
-
-const getEgg = (hen) =>
-    new Promise((resolve, reject) => {
-        setTimeout(() => reject(new Error('egg is not exist')));
-        // resolve(`${hen} => '🥚'`), 1000);
-    });
-
-const getFry = (egg) =>
-    new Promise((resolve, reject) => {
-        setTimeout(() => resolve(`${egg} => '🥯'`), 1000);
-    });
-
-getHen()
-    .then((hen) => getEgg(hen))
-    .catch((error) => {
-        return '🍕';
-    })
-    .then((egg) => getFry(egg))
-    .then((meal) => console.log(meal));
 {
-    function delay(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-    }
+    const start = new Promise((resolve) =>
+        setTimeout(() => {
+            console.log('start!!!');
+            resolve('start');
+        }, 2000)
+    );
 
-    function getApple() {
-        return delay(2000).then(() => '🍎');
-    }
+    const a = (value) => {
+        return new Promise((resolve) =>
+            setTimeout(() => {
+                console.log(`from ${value} to a`);
+                resolve('a');
+            }, 1500)
+        );
+    };
 
-    function getBanana() {
-        return delay(1000).then(() => '🍌');
-    }
+    const b = (value) => {
+        return new Promise((resolve) =>
+            setTimeout(() => {
+                console.log(`from ${value} to b`);
+                resolve('b');
+            }, 1000)
+        );
+    };
 
-    function pickFruits() {
-        return getApple().then((apple) => {
-            return getBanana().then((banana) => `${apple} + ${banana}`);
-        });
-    }
+    const c = (value) => {
+        return new Promise((resolve) =>
+            setTimeout(() => {
+                console.log(`from ${value} to c`);
+                resolve('c');
+            }, 1000)
+        );
+    };
 
-    pickFruits().then(console.log);
+    start
+        .then(a) //
+        .then(b)
+        .then((value) => c(value))
+        .finally((data) => console.log(data || 'end!!!', 'from finally'));
 }
+
+// {
+//     const getHen = () =>
+//         new Promise((resolve, reject) => {
+//             setTimeout(() => resolve('🐔'), 1000);
+//         });
+
+//     const getEgg = (hen) =>
+//         new Promise((resolve, reject) => {
+//             setTimeout(() => reject(new Error('egg is not exist')));
+//             // resolve(`${hen} => '🥚'`), 1000);
+//         });
+
+//     const getFry = (egg) =>
+//         new Promise((resolve, reject) => {
+//             setTimeout(() => resolve(`${egg} => '🥯'`), 1000);
+//         });
+
+//     getHen()
+//         .then((hen) => getEgg(hen))
+//         .catch((error) => {
+//             return '🍕';
+//         })
+//         .then((egg) => getFry(egg))
+//         .then((meal) => console.log(meal));
+//     {
+//         function delay(ms) {
+//             return new Promise((resolve) => setTimeout(resolve, ms));
+//         }
+
+//         function getApple() {
+//             return delay(2000).then(() => '🍎');
+//         }
+
+//         function getBanana() {
+//             return delay(1000).then(() => '🍌');
+//         }
+
+//         function pickFruits() {
+//             return getApple().then((apple) => {
+//                 return getBanana().then((banana) => `${apple} + ${banana}`);
+//             });
+//         }
+
+//         pickFruits().then(console.log);
+//     }
+// }
