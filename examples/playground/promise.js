f3ff4518288a0c5dbd71231d226ed64f5f7d1af5
Junha <prodopark15@gmail.com>
2022-02-06 00:02:59 +0900

    update string,number prototpye method

---
+++ examples/playground/promise.js
@@ -0,0 +1,3 @@
+const promise1 = new Promise(executor);
+
+function executor(resolve, recjet) {}
--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-const examples = [
-  '1.callback',
-  '2.callback-to-promise',
-  '3.promise',
-  '4.async',
-  '5.json',
-  '6.arrayAPIs',
-  '7.class',
-  '8.object',
-  '9.operator',
-  '10.tips',
-  '11.type',
-  'temp',
-];
-
-const addButton = (args) => {
-  args.forEach((element) => {
-    const button = document.createElement('button');
-    button.innerText = element;
-    button.addEventListener('click', () => addScript(element));
-    document.querySelector('body').appendChild(button);
-  });
-};
-
-const addScript = (element) => {
-  const script = document.createElement('script');
-  script.setAttribute('src', `examples/${element}.js`);
-  document.querySelector('head').appendChild(script);
-};
-
-addButton(examples);
-
-function getFormData() {
-  // state의 각 필드에 있는 value를 모아 하나의 객체로 리턴합니다.
-  // { name : 'Kim', age: 30 } 의 형식으로 리턴해야 합니다.
-  return Object.entries(formState).reduce(
-    (formData, [key, { value, validator }]) => ({ ...formData, [key]: value })
-  );
+function test() {
+  return {
+    rs: 25,
+    bar: () => {
+      console.log(this.rs);
+    },
+  };
 }
+const obj = test.call({ rs: 100 }); // {rs:25, bar:function}
+obj.bar(); // 100
