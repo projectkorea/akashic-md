383af02afe5753eb8c34d20db5bb62e05587c691
unknown <prodopark15@gmail.com>
2024-05-19 20:46:34 +0900

    feat(java): variable, class

---
+++ src/main/java/com/projectkorea/step01/Step01Main.java
@@ -0,0 +1,13 @@
+package com.projectkorea.step01;
+
+public class Step01Main {
+
+    public static void main(String[] args){
+        long num1 = 10L;
+        final long num2 = 10L;
+        Long num3 =1_000L;
+        Person person = new Person("junha");
+
+        System.out.println(person.getName());
+    }
+}
