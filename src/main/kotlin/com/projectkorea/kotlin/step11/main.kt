f1be615aba66f28c0f45a513265b8557a614ec9f
unknown <prodopark15@gmail.com>
2024-06-06 20:25:48 +0900

    feat(kotlin): object 키워드

--- src/main/kotlin/com/projectkorea/kotlin/step11/main.kt
+++ src/main/kotlin/com/projectkorea/kotlin/step11/main.kt
@@ -3,7 +3,6 @@ package com.projectkorea.kotlin.step11
 class main {
 }
 
-
 // 클래스 안의 멤버
 // 생성자에 접근 지시어를 추가 해주려면 constructor 을 써야함
 
@@ -23,25 +22,20 @@ open class Cat protected constructor(val name: String) {
 // protected 에서는 패키지가 빠지고
 // default 는 internal 로 변경되면서 모듈 접근 제어하는 기능이 생김
 
-
 // default 가 public
-// protected는 파일 최상단에서 쓸 수 없음, 선언된 클래스와 하위 클래스에 해당하는 개념
-// private는 같은 파일 내에서만 사용 가능
+// protected 는 파일 최상단에서 쓸 수 없음, 선언된 클래스와 하위 클래스에 해당하는 개념
+// private 는 같은 파일 내에서만 사용 가능
 
 
-/* 접근 가능한 곳
-public: 모든 곳
-protected: 선언된 클래스, 하위 클래스
-internal: 같은 모듈 내부
-private: 선언된 클래스 내부
-*/
-
+// 접근 가능한 곳
+// public: 모든 곳
+// protected: 선언된 클래스, 하위 클래스
+// internal: 같은 모듈 내부
+// private: 선언된 클래스 내부
 
 // Java Kotlin 함께 쓸 때 주의점
 // 1. internal 은 Java 에서 public 으로 취급 -> Java 코드에서는 Kotlin 모듈의 internal 코드를 가져올 수 있음
 // 2. Java 는 같은 패키지의 Kotlin protected 멤버에 접근 가능
-
-
 
 // 요약
 // 1. 코틀린 에서 패키지는 네임스페이스 관리용. protected 의미 달라짐.
@@ -49,4 +43,4 @@ private: 선언된 클래스 내부
 // 3. 생성자에 접근 지시어를 붙일 때는 constructor 를 명시적으로 써야함
 // 4. 유틸성 함수를 만들 때는 파일 최상단 이용
 // 5. 프로퍼티의 custom setter 에 접근 지시어 붙이기 가능
-// 6. Java 에서 Kotlin 코드를 사용할 때 internal 과 protected 는 주의하자
+// 6. 자바에서 코틀린 코드를 사용할 때 internal 과 protected 는 주의하자
---
+++ src/main/kotlin/com/projectkorea/kotlin/step12/main.kt
@@ -0,0 +1,10 @@
+package com.projectkorea.kotlin.step12
+
+class main {
+}
+
+// 요약
+// 1. Java의 static 변수와 함수를 만드려면, 코틀린에서는 companion object 를 사용한다.
+// 2. companion object 도 하나의 객체다. 이름을 붙일 수 있고, 다른 타입을 상속받을 수 있다.
+// 3. 코틀린에서 싱글톤 클래스를 만들 때 object 키워드를 사용한다.
+// 4. 익명 클래스를 만들 때 object : 타입을 사용한다.
