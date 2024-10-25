aab15072773294bc51d55e13e7e9e2d362c91978
kim leo <prodopark15@gmail.com>
2024-10-25 22:49:27 +0900

    feat(js): callback with promise

---
+++ javascript/lesson/promise.js
@@ -0,0 +1,19 @@
+// read, write -> 동기적으로 실행되는 코드
+import {readFile, writeFile} from 'fs';
+
+console.log("1");
+
+writeFile('bori.txt', 'bori bye bye', null, (err)=>{
+  console.log("2","write done");
+  if (err) {
+    console.log("3", err);
+  }
+})
+
+console.log("4");
+
+readFile('bori2.txt', 'utf-8', (err, data)=> {
+  console.log("5", data);
+})
+
+console.log("6");
