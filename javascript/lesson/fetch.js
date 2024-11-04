39be65be3896350e1bc9c5ec6608739b35f03e9b
kim leo <prodopark15@gmail.com>
2024-11-04 22:46:50 +0900

    feat(js): onFulfilled 보충

--- javascript/lesson/fetch.js
+++ javascript/lesson/fetch.js
@@ -1,7 +1,9 @@
 const url = 'https://api.thecatapi.com/v1/images/search'
+// json 포맷의 데이터를 주는 주소
 
 fetch('https://api.thecatapi.com/v1/images/search') //
   .then((response) => {
+    // response는 응답에 관한 객체가 반환이 된다.
     console.log(response)
     return response.json() // json is also async
   })
